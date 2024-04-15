5423c543a25f13aacc964f88f2ed21e56b348c6f
Chinmay Agrawal <chinmayagrawal775@gmail.com>
2024-04-15 19:36:01 +0530

    Update: Settings.py to deploy project.

--- canteen_ordering_sys/settings.py
+++ canteen_ordering_sys/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'canteen_ordering_sys.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "canteen_db",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
+    
 }
 
 
@@ -123,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA_URL= 'food_pic/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'food_pic')
