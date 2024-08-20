fbc8a226627f2605db0a6a93dc96f7a3b4f72ff8
Chinmay Agrawal <chinmayagrawal775@gmail.com>
2024-08-20 20:13:24 +0530

    Update: setting.py
    Update local configs

--- canteen_ordering_sys/settings.py
+++ canteen_ordering_sys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2k8svw_k2ylfsr##^cjh9^33m=_v+s%c&&3ge149i&&-spx+-8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["chinmay.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1","chinmay.pythonanywhere.com"]
 
 
 # Application definition
@@ -76,18 +76,18 @@ WSGI_APPLICATION = "canteen_ordering_sys.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "canteen_db",
-        "USER": "admin",
-        "PASSWORD": "admin",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "canteen_db",
+    #     "USER": "admin",
+    #     "PASSWORD": "admin",
+    #     "HOST": "127.0.0.1",
+    #     "PORT": "5432",
+    # }
 }
 
 
